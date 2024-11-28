3f83f73dd1be72673f29456f64be5c0ca7dc308b
Pektro <pedrorafael.drt@gmail.com>
2024-11-28 11:58:40 +0000

    added communication functions

--- node.py
+++ node.py
@@ -1,24 +1,121 @@
+import random
+import socket
+import threading
+import signal
+import sys
+import time
 
 class Node:
     def __init__(self, id, x, y):
         self.id = id
-        self.x  = x
-        self.y  = y
-        self.state = "Supscetible"      # "Supscetible", "Infected" or "Removed"
+        self.x = x
+        self.y = y
+        self.state = "Susceptible"  # "Susceptible", "Infected" or "Removed"
         self.neighbors = []
+        self.prob_infection = 1
+        self.stop_event = threading.Event()
+
+        ''' Create Socket > Bind > Get Port > Start Listening '''
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('localhost', 0))
+        self.port = self.socket.getsockname()[1]
+
+        self.listen_thread = threading.Thread(target=self.accept_connection, args=[self.socket])
+        self.listen_thread.start()
+
+        print(f"Node {self.id} is listening on port {self.port}")
+
+    def __str__(self):
+        nbs = [neighbor.id for neighbor in self.neighbors]
+        return f"Node {self.id}: {self.state}\nNeighbors: {nbs}"
+
+    ''''''''''''''''''''''''''''''''''''''
+    '''    COMMUNICATION FUNCTIONS     '''
+    ''''''''''''''''''''''''''''''''''''''
+    def accept_connection(self, socket):
+        socket.listen(5)
+        while not self.stop_event.is_set():
+            try:
+                conn, addr = socket.accept()
+                threading.Thread(target=self.handle_connection, args=(conn, addr)).start()
+            except socket.timeout:
+                continue
+            except OSError:
+                break
+
+    def handle_connection(self, conn, addr):
+        while not self.stop_event.is_set():
+            try:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                message = data.decode()
+                print(f"Node {self.id} received data: {message}")
+            except socket.timeout:
+                continue
+            except OSError:
+                break
+        conn.close()
+
+    def send_message(self, message, neighbor):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+                conn.connect(('localhost', neighbor.port))
+                conn.send(message.encode())
+                print(f"Node {self.id} sent message to Node {neighbor.id}: {message}")
+        except Exception as e:
+            print(f"Node {self.id} failed to send message to Node {neighbor.id}: {e}")
+
+    def stop(self):
+        self.stop_event.set()
+        self.socket.close()
+        self.listen_thread.join()
+
+    ''''''''''''''''''''''''''''''''
+    '''    PROTOCOL FUNCTIONS    '''
+    ''''''''''''''''''''''''''''''''
 
     def add_neighbor(self, neighbor):
         self.neighbors.append(neighbor)
 
     def push(self):
-        pass
+        # random int for nr of neighbors
+        n = random.randint(0, len(self.neighbors)-1)
+        neighbor_to_push = self.neighbors[n]
 
-    def pull(self):
-        pass
-
-    def push_pull(self):
+        self.set_timeout()
         pass
 
     def gossip_protocol(self):
         pass
-        
+
+if __name__ == "__main__":
+    nodes = []
+
+    def signal_handler(sig, frame):
+        print("Ctrl+C pressed, stopping all nodes...")
+        for node in nodes:
+            node.stop()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    node1 = Node(1, 0, 0)
+    node2 = Node(2, 1, 1)
+    nodes.append(node1)
+    nodes.append(node2)
+
+    node1.add_neighbor(node2)
+    node2.add_neighbor(node1)
+
+    node1.send_message("Hello", node2)
+    node2.send_message("Hi", node1)
+    node1.send_message("How are you?", node2)
+    node2.send_message("I'm fine", node1)
+
+    # Keep the main thread running
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        signal_handler(None, None)
--- simulation.py
+++ simulation.py
@@ -1,33 +1,54 @@
+import signal
+import sys
+import time
+from random import random
 from node import Node
-import random
 
-class Simulation():
-
-    def __init__(self, num_nodes, num_nbr, num_nbr_byz, byz_percent):
-
-        self.num_nodes   = num_nodes
-        self.num_nbr     = num_nbr
-        self.num_nbr_byz = num_nbr_byz
-        self.byz_percent = byz_percent
-
+class Simulation:
+    def __init__(self, num_nodes, num_nbr, prob_infection, delay_prob):
+        self.num_nodes = num_nodes
+        self.num_nbr = num_nbr
+        self.prob_infection = prob_infection
+        self.delay_prob = delay_prob
         self.nodes = []
-        
         self.create_nodes()
         self.generate_neighbors()
 
     def create_nodes(self):
         for i in range(self.num_nodes):
-            node = Node(i, i%10, i//10)
+            node = Node(i, i % 10, i // 10)
             self.nodes.append(node)
 
     def generate_neighbors(self):
         for i in range(self.num_nodes):
-            for j in range(i+1, self.num_nodes):
-                if random(0, 1) < self.num_nbr/self.num_nodes:
+            for j in range(i + 1, self.num_nodes):
+                if random() < self.num_nbr / self.num_nodes:
                     self.nodes[i].add_neighbor(self.nodes[j])
                     self.nodes[j].add_neighbor(self.nodes[i])
 
-if __name__ == "__main__":
-    sim = Simulation(100, 4, 1, 0.1)
-    for i in range(10):
+    def stop_simulation(self):
+        print("Stopping simulation...")
+        for node in self.nodes:
+            node.stop()
 
+if __name__ == "__main__":
+    def signal_handler(sig, frame):
+        sim.stop_simulation()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    sim = Simulation(num_nodes=100, num_nbr=4, prob_infection=1, delay_prob=0.1)
+
+    print(sim.nodes[0])
+
+    node1 = sim.nodes[0]
+
+    node1.send_message("Hello", node1.neighbors[0])
+
+    # Keep the main thread running
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        signal_handler(None, None)
